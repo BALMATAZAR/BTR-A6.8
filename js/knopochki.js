6589097f207e727b5f289c7c9236f2ddaab8091b
BALMATAZAR <57841120+BALMATAZAR@users.noreply.github.com>
2019-12-02 22:59:23 +0300

    Add files via upload

---
+++ js/knopochki.js
@@ -0,0 +1,17 @@
+let i = 0;
+function cl1() {
+    i = i + 1;
+	$("#my-progress-bar").width(i+'%');
+}
+
+function cl2() {
+    i = i + 3;
+	$("#my-progress-bar").width(i+'%');
+}
+
+function cl3() {
+    i = i + 7;
+	$("#my-progress-bar").width(i+'%');
+}
+
+
